9d0e487c553b9f81aaa56e9eb8533501b4bcffe4
Thorinair <thorinair@yahoo.com>
2016-05-07 19:33:36 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,12 @@
+# Description
+
+
+# Installing
+**Prerequisites**
+- Minecraft 1.7.10
+- Forge 10.13.4.1448 (or later)
+
+# Building
+**Prerequisites**
+- Minecraft 1.7.10
+- Forge 10.13.4.1448
---
+++ main/java/com/example/examplemod/Viveporter.java
@@ -0,0 +1,20 @@
+package com.example.examplemod;
+
+import net.minecraft.init.Blocks;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = Viveporter.MODID, version = Viveporter.VERSION)
+public class Viveporter
+{
+    public static final String MODID = "viveporter";
+    public static final String VERSION = "0.1.0";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+		// some example code
+        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+    }
+}
